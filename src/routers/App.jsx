27268a5c9219306d7c96059b0f3008ce1dba0582
rasuljon313 <anvarovrasuljon313@gmail.com>
2025-01-27 10:25:53 +0500

    add

--- src/routers/App.jsx
+++ src/routers/App.jsx
@@ -1,4 +1,9 @@
-import { Route, Router, Routes } from "react-router-dom";
+import {
+  BrowserRouter,
+  Route,
+  //  Router,
+  Routes,
+} from "react-router-dom";
 import Login from "../Page/Login";
 import Category from "../Page/Category";
 import Brand from "../Page/Brand";
@@ -6,14 +11,14 @@ import Product from "../components/Product";
 
 const App = () => {
   return (
-    <Router>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/category" element={<Category />} />
         <Route path="/brand" element={<Brand />} />
         <Route path="/product" element={<Product />} />
       </Routes>
-    </Router>
+    </BrowserRouter>
   );
 };
 
